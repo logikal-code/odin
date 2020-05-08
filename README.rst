d3dba9395342226e81c47f7cb6019cbaaa0d632f
GergelyKalmar <gergely.kalmar@logikal.jp>
2020-05-08 17:50:09 +0200

    Add initial files

---
+++ README.rst
@@ -0,0 +1,11 @@
+Odin
+====
+A public shell script for initializing Logikal user machines.
+
+Usage
+-----
+You can initialize your system by simply executing the following commands::
+
+    wget https://raw.githubusercontent.com/logikal-jp/odin/master/init.sh
+    chmod +x init.sh
+    ./init.sh -u <your GitHub user> -e <your GitHub email>
---
+++ init.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+_abort() { echo 'Aborted' && exit 1; }
+_error() {
+    if [[ $1 != "" ]]; then
+        echo "$0: error: $1"
+    fi
+    echo "usage: $0 -u GitHubUser -e GitHubEmail"
+    exit 1
+}
+
+# Processing command-line arguments
+while getopts u:e: OPT; do
+    case ${OPT} in
+        u) user=${OPTARG};;
+        e) email=${OPTARG};;
+        \?) _error;;
+    esac
+done
+
+if [[ "${user}" == "" || "${email}" == "" ]]; then
+    _error 'invalid arguments'
+fi
+
+# Generating SSH key
+keyfile=~/.ssh/id_rsa
+if [[ ! -f "${keyfile}" ]]; then
+    ssh-keygen -f "${keyfile}" -t rsa -b 4096 -C "${email}" || _abort
+fi
+
+echo 'Updating system'
+sudo apt update && sudo apt upgrade --yes || _abort
+
+echo 'Installing packages'
+sudo snap install chromium
+sudo apt install --yes git ansible
+
+echo 'Configuring git'
+git config --global user.name "${user}"
+git config --global user.email "${email}"
+git config --global push.followTags true
+
+echo 'Configuring ansible'
+file=/etc/ansible/hosts
+line='localhost ansible_connection=local'
+sudo grep -xqF -- "${line}" "${file}" || echo "${line}" | sudo tee --append "${file}" > /dev/null
+
+echo 'Creating projects folder'
+mkdir -p ~/Projects
+
+echo 'Initialization complete'
